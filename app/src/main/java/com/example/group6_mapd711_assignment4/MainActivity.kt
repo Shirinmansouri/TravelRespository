25e3308051c9825f4d03c5c28be9d5d164696968
Shirin <shirin.mansori@gmail.com>
2021-11-17 01:14:19 -0500

    fourth commit

--- app/src/main/java/com/example/group6_mapd711_assignment4/MainActivity.kt
+++ app/src/main/java/com/example/group6_mapd711_assignment4/MainActivity.kt
@@ -1,11 +1,18 @@
 package com.example.group6_mapd711_assignment4
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+    fun createNewCustomer(view: View)
+    {
+        val intent= Intent(this,CreateCustomerActivity::class.java)
+startActivity(intent)
     }
 }
