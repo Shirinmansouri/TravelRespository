19d55ae55cbee43a085cd28f5ddba2c8322f2d7f
Shirin <shirin.mansori@gmail.com>
2021-11-17 00:17:49 -0500

    third commit

--- app/src/main/java/com/example/group6_mapd711_assignment4/BookingViewModel.kt
+++ app/src/main/java/com/example/group6_mapd711_assignment4/BookingViewModel.kt
@@ -10,6 +10,7 @@ class BookingViewModel : ViewModel() {
     // calling repository tasks and
     // sending the results to the Activity
     var liveDataBook: LiveData<BookingModel>? = null
+    var l : LiveData<BookingModel>? = null
 
     //
     fun insertBooking(context: Context, numberOfAdults: String, numberOfKids: String,
@@ -25,5 +26,9 @@ class BookingViewModel : ViewModel() {
     fun getBookings(context: Context,cruiseCode: Int?, customerId: Int?) : LiveData<BookingModel>? {
         liveDataBook = BookingRepository.getBookings(context, cruiseCode, customerId)
         return liveDataBook
+    }
+    fun getBookingsByCustomer(context: Context, customerId: Int?) : LiveData<BookingModel>? {
+        l = BookingRepository.getBookingsByCustomer(context, customerId)
+        return l
     }
 }
