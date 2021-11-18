561ce4b97fd36caa96751d270d154e37a10bb351
Shirin <shirin.mansori@gmail.com>
2021-11-18 00:10:30 -0500

    Color commit

---
+++ app/src/main/java/com/example/group6_mapd711_assignment4/EditCustomerActivity.kt
@@ -0,0 +1,11 @@
+package com.example.group6_mapd711_assignment4
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EditCustomerActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit_customer)
+    }
+}
